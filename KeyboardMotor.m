e937794ab812de56c4dc556e6c70703bb3a9d524
VaYNC0DE <145499473+VaYNC0DE@users.noreply.github.com>
2023-10-22 09:17:59 -0700

    Add files via upload

--- KeyboardMotor.m
+++ KeyboardMotor.m
@@ -6,27 +6,32 @@ while (1)
     pause(0.1);
     switch key
         case 'uparrow'
-            disp ("Up arrow pressed");
+            %disp ("Up arrow pressed");
             brick.MoveMotorAngleRel('A', 100, 360, 'Coast');
             brick.MoveMotorAngleRel('B', 100, 360, 'Coast');
             
         case 'downarrow'
-            disp("Down arrow pressed");
+            %disp("Down arrow pressed");
             brick.MoveMotorAngleRel('A', 100, -360, 'Coast');
             brick.MoveMotorAngleRel('B', 100, -360, 'Coast');
             
-        case 'leftarrow'
-            disp("left arrow pressed");
-            brick.ResetMotorAngle('B');
-            brick.MoveMotorAngleRel('B', 55, 90, 'Brake');
-            
         case 'rightarrow'
+            %disp("left arrow pressed");q
             brick.ResetMotorAngle('A');
             brick.MoveMotorAngleRel('A', 55, 90, 'Brake');
             
+        case 'leftarrow'
+            brick.ResetMotorAngle('B');
+            brick.MoveMotorAngleRel('B', 55, 90, 'Brake');
+            
         
-        case 0
-            disp("No key pressed");
+        case 'r'
+            brick.ResetMotorAngle('D');
+            brick.MoveMotorAngleRel('D', 20, 90, 'Brake');
+        
+        case 't'
+            brick.ResetMotorAngle('D');
+            brick.MoveMotorAngleRel('D', 20, -90, 'Brake');
         
         case 'q'
             brick.StopMotor('B');
