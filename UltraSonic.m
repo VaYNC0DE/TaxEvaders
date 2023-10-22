32b79282cb8c9d5a67dfaa08e033d9114f6d31e2
VaYNC0DE <145499473+VaYNC0DE@users.noreply.github.com>
2023-10-22 09:19:22 -0700

    Add files via upload

--- UltraSonic.m
+++ UltraSonic.m
@@ -2,4 +2,21 @@ distance = brick.UltrasonicDist(4);
 
  % Get distance to the nearest object.
 
-display(distance);                   % Print distance.
+% add || statement. add multiple if or switch cases
+while (distance ~= 255)
+    if (distance < 15 && distance > 18)
+        display(" the IF distance " + distance);
+        brick.MoveMotor('A', 40);
+        brick.MoveMotor('B', 40);
+    else (distance > 15 && distance < 18)
+        display("the else distance "  + distance);
+        brick.ResetMotorAngle('A');
+        brick.MoveMotorAngleRel('A', 55, -90, 'Brake');
+    end
+end
+
+
+
+
+
+%display(distance);                   
