ed1fb5206e5328d9fc81b5af6ad6a7af0066697a
VaYNC0DE <145499473+VaYNC0DE@users.noreply.github.com>
2023-11-14 10:49:06 -0700

    Add files via upload

--- Main.m
+++ Main.m
@@ -3,8 +3,9 @@ while true
     color = brick.ColorCode(2);
     distance = brick.UltrasonicDist(1);
     reading = brick.TouchPressed(3);
-    brick.MoveMotor('A',25);
-    brick.MoveMotor('B',25);
+    readingTwo = brick.TouchPressed(4);
+    brick.MoveMotor('A',50);
+    brick.MoveMotor('B',50);
     if color == 5 %red
         %pause(1);
         brick.StopMotor('AB');
@@ -26,15 +27,20 @@ while true
         KeyboardMotor;
         pause(2);
     end
-    if reading == 1
+    if reading == 1 || readingTwo == 1
         brick.MoveMotor('AB' , -30);
         pause(1);
-        brick.MoveMotor('A', 15);
+        brick.MoveMotor('A', 30 );
         pause(1);
     end
     if distance > 100 %too close to wall
         brick.StopMotor('A');
-        brick.MoveMotor('B', 15);
+        brick.MoveMotor('B', 30);
+        pause(2);
+    end
+    if distance < 3 %move the other direction
+        brick.StopMotor('B');
+        brick.MoveMotor('A', 30);
         pause(2);
     end
 end
